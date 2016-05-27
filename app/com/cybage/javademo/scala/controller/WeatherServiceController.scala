3a423b4975ec4c7df9b83f498e82e9c03518cf62
Sumit Kumar <er.sumit.kr12@gmail.com>
2016-05-27 15:04:47 +0530

    added per day forecast feature in WeatherServiceController and updated routes too

--- app/com/cybage/javademo/scala/controller/WeatherServiceController.scala
+++ app/com/cybage/javademo/scala/controller/WeatherServiceController.scala
@@ -19,6 +19,13 @@ object WeatherServiceController extends Controller {
   val urlPrefix = "http://api.openweathermap.org/data/2.5/forecast?q="
   val urlPostfix = "&units=Metric&APPID="
   val tokenKey = "026bae7a696c12547b424cb730ff80e4"
+  
+  
+  val urlPrefixDayWise="http://api.openweathermap.org/data/2.5/forecast/daily?q="
+   // pune
+   val urlPostfixDayWise="&mode=json&units=metric&cnt="
+     // 10
+     val tokenKeyDayWise="&appid=026bae7a696c12547b424cb730ff80e4"
 
   /**
    * forecastWeather: Getting called from  conf/routes file, having city as query string to be called using WS.get(URL)
@@ -26,7 +33,7 @@ object WeatherServiceController extends Controller {
    * Getting response object from Await.result who is ensuring it will try to get the response until
    * the time reaches Double.PositiveInfinity(Duration.Inf)
    */
-  def forecastWeather(city: String) = Action.async {
+  def forecastByCity(city: String) = Action.async {
 
     implicit request =>
 
@@ -47,6 +54,28 @@ object WeatherServiceController extends Controller {
         resp.map { response => Ok(response.statusText).withHeaders(ACCESS_CONTROL_ALLOW_ORIGIN -> "*")}
       }
 
+  }
+  def forecastByCityAndDays(city: String,days:Int) = Action.async {
+
+    implicit request =>
+      val requestTime = Calendar.getInstance().getTime().getTime
+      
+      Logger.debug("Request => " + "Request ID :" + request.id + ", Method :" + request.method + ", Path :" + request.path + ", Host :" + request.host + ", Remote Address: " + request.remoteAddress + ", Remote Version: " + request.version)
+      println("==>URL : "+urlPrefixDayWise + city + urlPostfixDayWise+days.toString()+ tokenKeyDayWise)
+      val resp = WS.url(urlPrefixDayWise + city + urlPostfixDayWise+days.toString()+ tokenKeyDayWise).get()
+      val response = Await.result(resp, Duration.Inf)
+
+      if (response.status == 200) {
+        //total processing time 
+        val totalRequestProcessingTime = ((Calendar.getInstance().getTime().getTime) - requestTime)
+        Logger.info("Response =>" + response.json+ "totalRequestProcessingTime" + totalRequestProcessingTime + "ms")
+        resp.map { response => Ok((response.json)).withHeaders(ACCESS_CONTROL_ALLOW_ORIGIN -> "*")}
+      } else {
+        Logger.info("Response =>" + response.body.toString())
+        resp.map { response => Ok(response.statusText).withHeaders(ACCESS_CONTROL_ALLOW_ORIGIN -> "*")}
+      }
+      
+      
   }
 
 }
